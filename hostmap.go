bf9310c7acab26d34716833a867be1c6ec6b65bf
Dominic Marks <djwmarks@gmail.com>
2020-08-05 12:27:56 +1000

    add 100.64/10 prefix; RFC6598

--- hostmap.go
+++ hostmap.go
@@ -801,8 +801,9 @@ func localIps(allowList *AllowList) *[]net.IP {
 func PrivateIP(ip net.IP) bool {
 	private := false
 	_, private24BitBlock, _ := net.ParseCIDR("10.0.0.0/8")
+	_, private22BitBlock, _ := net.ParseCIDR("100.64.0.0/10")
 	_, private20BitBlock, _ := net.ParseCIDR("172.16.0.0/12")
 	_, private16BitBlock, _ := net.ParseCIDR("192.168.0.0/16")
-	private = private24BitBlock.Contains(ip) || private20BitBlock.Contains(ip) || private16BitBlock.Contains(ip)
+	private = private24BitBlock.Contains(ip) || private22BitBlock.Contains(ip) || private20BitBlock.Contains(ip) || private16BitBlock.Contains(ip)
 	return private
 }
